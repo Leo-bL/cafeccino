fc3330f213b082b3a25b3bbe5ca4ca481ec6a971
Leo-bL <hamode.alblati@gmail.com>
2025-08-20 00:15:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,51 @@
+
+const btn_menu = document.getElementById('b-menu');
+const bars = document.querySelectorAll('.bar')
+const the_menu = document.querySelector('.the-menu');
+const links = document.querySelectorAll('.link');
+btn_menu.onclick = function(){
+    bars[0].classList.toggle('translate-y-[150%]');
+    bars[1].classList.toggle('-translate-y-[150%]');
+    bars[0].classList.toggle('rotate-45');
+    bars[1].classList.toggle('-rotate-45');
+    the_menu.classList.toggle('mt-[10%]');
+    setTimeout(() => {
+        the_menu.classList.toggle('h-[200%]');
+        
+    }, 100);
+    setTimeout(() => {
+        for (let i = 0; i < links.length; i++) {
+            links[i].classList.toggle('hidden')
+            setTimeout(() => {
+                links[i].classList.toggle('opacity-100');
+            }, i * 100);
+        }
+    }, 200);
+    
+}
+
+const beans = document.getElementById('beans');
+window.onload = function () {
+    setTimeout(() => {
+        beans.classList.remove('animation-open-up');
+    }, 1500);
+}
+
+let indexia = 0;
+const caption = document.getElementById('caption');
+let caption_array = ['A cup, a moment, a life.',' is the first of all','it`s the best part of waking up ']
+setInterval(() => {
+    caption.classList.add('opacity-0');
+    caption.classList.add('translate-y-[150%]');
+    caption.classList.remove('text-mybrawon');
+    caption.classList.add('text-myblack');
+    setTimeout(() => {
+        caption.innerHTML = caption_array[indexia]
+    indexia++;
+    if (indexia > caption_array.length-1) {
+        indexia = 0;
+    }
+        caption.classList.remove('opacity-0');
+    caption.classList.remove('translate-y-[150%]');
+    }, 500);
+}, 4000);
